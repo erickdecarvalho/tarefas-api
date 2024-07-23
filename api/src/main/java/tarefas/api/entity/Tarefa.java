20dabb184a7c9d54c1e9d6b020eae6f18e6d1a72
erick7amorim <erickamorimcarv@gmail.com>
2024-07-23 13:51:19 -0300

    feat(Tarefa): criar modelo da entidade

---
+++ api/src/main/java/tarefas/api/entity/Tarefa.java
@@ -0,0 +1,66 @@
+package tarefas.api.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tarefas")
+public class Tarefa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String nome;
+    @NotBlank
+    private String descricao;
+    private boolean realizada;
+    private int prioridade;
+
+    public Tarefa(String nome, String descricao, boolean realizada, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizada = realizada;
+        this.prioridade = prioridade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizada() {
+        return realizada;
+    }
+
+    public void setRealizada(boolean realizada) {
+        this.realizada = realizada;
+    }
+
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
